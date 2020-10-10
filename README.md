02e52f8ef6e4019bb9f428ed94fdb7f9eb1ba197
ericma <924402086@qq.com>
2020-10-10 13:54:15 +0800

    init

--- README.md
+++
@@ -1,2 +0,0 @@
-# springBootDemo
-spring boot demo
---
+++ src/main/java/com/springboot/demo/controller/L01HelloWorld.java
@@ -0,0 +1,12 @@
+package com.springboot.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class L01HelloWorld {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello world!!!";
+    }
+}
