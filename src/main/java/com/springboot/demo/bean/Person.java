5497032314444ca384a6c9532dcc7b4a1b7f6d40
ericma <924402086@qq.com>
2020-10-10 14:32:00 +0800

    添加了单元测试

---
+++ src/main/java/com/springboot/demo/bean/Person.java
@@ -0,0 +1,89 @@
+package com.springboot.demo.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@ConfigurationProperties(prefix = "person")
+public class Person {
+    private String lastName;
+    private Integer age;
+    private Boolean boss;
+    private Date birth;
+    private Map<String, Object> maps;
+    private List<Object> lists;
+    private Dog dog;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", boss=" + boss +
+                ", birth=" + birth +
+                ", maps=" + maps +
+                ", lists=" + lists +
+                ", dog=" + dog +
+                '}';
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setBoss(Boolean boss) {
+        this.boss = boss;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public void setMaps(Map<String, Object> maps) {
+        this.maps = maps;
+    }
+
+    public void setLists(List<Object> lists) {
+        this.lists = lists;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Boolean getBoss() {
+        return boss;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public Map<String, Object> getMaps() {
+        return maps;
+    }
+
+    public List<Object> getLists() {
+        return lists;
+    }
+
+    public Dog getDog() {
+        return dog;
+    }
+}
--- src/test/java/com/springboot/demo/DemoApplicationTests.java
+++ src/test/java/com/springboot/demo/DemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.springboot.demo;
 
+import com.springboot.demo.bean.Person;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+    @Autowired
+    Person person;
+
     @Test
     void contextLoads() {
+        System.out.println(person);
     }
 
 }
