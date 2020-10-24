e568910809328e8199b704a887468cacc6955e44
ericma <924402086@qq.com>
2020-10-24 20:00:41 +0800

    logger

--- src/main/java/com/springboot/demo/controller/L01HelloWorld.java
+++ src/main/java/com/springboot/demo/controller/L01HelloWorld.java
@@ -3,6 +3,7 @@ package com.springboot.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class L01HelloWorld {
     @GetMapping("/hello")
--- src/test/java/com/springboot/demo/DemoApplicationTests.java
+++ src/test/java/com/springboot/demo/DemoApplicationTests.java
@@ -3,11 +3,15 @@ package com.springboot.demo;
 import com.springboot.demo.bean.Person;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
+
+    Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
     Person person;
@@ -15,6 +19,12 @@ class DemoApplicationTests {
     @Test
     void contextLoads() {
         System.out.println(person);
+        // error > warn > info >　debug > trace
+        logger.error("显示 error");
+        logger.warn("显示 warn");
+        logger.info("显示 info");
+        // springboot 默认是 root 从 info 开始显示
+        logger.debug("显示 debug");
+        logger.trace("显示 trace");
     }
-
 }
