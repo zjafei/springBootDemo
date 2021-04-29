e34942e68e233d2c8a697564d601d2e491e40427
ericma <924402086@qq.com>
2021-04-29 14:34:24 +0800

    添加了localeResolver实现了国际化

--- src/main/java/com/springboot/demo/config/MyMvcConfig.java
+++ src/main/java/com/springboot/demo/config/MyMvcConfig.java
@@ -1,7 +1,9 @@
 package com.springboot.demo.config;
 
+import com.springboot.demo.component.MyLocaleResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -10,19 +12,25 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class MyMvcConfig implements WebMvcConfigurer {
     @Override
     public void  addViewControllers(ViewControllerRegistry registry){
-        registry.addViewController("/atguigu").setViewName("success");
+        registry.addViewController("/success").setViewName("success");
+        registry.addViewController("/").setViewName("login");
+        registry.addViewController("/index.html").setViewName("login");
     }
 
-    @Bean //添加到容器中
-    public WebMvcConfigurerAdapter webMvcConfigurerAdapter(){
-        WebMvcConfigurerAdapter adapter = new WebMvcConfigurerAdapter() {
-            @Override
-            public void addViewControllers(ViewControllerRegistry registry) {
-                registry.addViewController("/").setViewName("login");
-                registry.addViewController("/index.html").setViewName("login");
-            }
-        };
-        return  adapter;
-    }
+//    @Bean //添加到容器中
+//    public WebMvcConfigurerAdapter webMvcConfigurerAdapter(){
+//        WebMvcConfigurerAdapter adapter = new WebMvcConfigurerAdapter() {
+//            @Override
+//            public void addViewControllers(ViewControllerRegistry registry) {
+//                registry.addViewController("/").setViewName("login");
+//                registry.addViewController("/index.html").setViewName("login");
+//            }
+//        };
+//        return  adapter;
+//    }
 
+    @Bean // 添加区域信息控制器到容器中
+    public LocaleResolver localeResolver(){
+        return  new MyLocaleResolver();
+    }
 }
