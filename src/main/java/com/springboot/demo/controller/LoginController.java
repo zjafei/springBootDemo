814ede8b3fc88ac3fb12152975189f10bf673026
ericma <924402086@qq.com>
2021-04-30 14:56:49 +0800

    添加了拦截器

--- src/main/java/com/springboot/demo/controller/LoginController.java
+++ src/main/java/com/springboot/demo/controller/LoginController.java
@@ -4,6 +4,8 @@ import org.springframework.util.StringUtils;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 // import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +17,11 @@ public class LoginController {
 
   // @RequestMapping(value = "/user/login", method = RequestMethod.POST)
   @PostMapping(value = "/user/login")
-  public String login(@RequestParam("username") String username, @RequestParam("password") String password, Map<String, Object> map ) {
+  public String login(@RequestParam("username") String username, @RequestParam("password") String password,
+      Map<String, Object> map, HttpSession session) {
     if (!StringUtils.isEmpty(username) && "123456".equals(password)) {
-      return "dashboard";
+      session.setAttribute("loginUser", username);
+      return "redirect:/main";
     } else {
       map.put("msg", "用户名密码错误");
       return "login";
