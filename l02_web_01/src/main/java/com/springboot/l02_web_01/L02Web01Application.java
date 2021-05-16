16ed38cafa3b2e9cb92d33cb82452686332f81ef
ericma <924402086@qq.com>
2021-05-16 09:02:24 +0800

    init

---
+++ l02_web_01/src/main/java/com/springboot/l02_web_01/L02Web01Application.java
@@ -0,0 +1,13 @@
+package com.springboot.l02_web_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L02Web01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L02Web01Application.class, args);
+	}
+
+}
---
+++ l02_web_01/src/test/java/com/springboot/l02_web_01/L02Web01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.l02_web_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L02Web01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
