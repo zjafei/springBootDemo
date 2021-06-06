640d7c41e93468b5efc60dfddc40966003a0214e
ericma <924402086@qq.com>
2021-06-06 16:40:33 +0800

    添加了通过自定义配置来添加servlet

---
+++ l05_web_admin/src/main/java/com/spring/l05_web_admin/config/MyRegistConfig.java
@@ -0,0 +1,29 @@
+package com.spring.l05_web_admin.config;
+
+// import java.util.Arrays;
+
+// import com.spring.l05_web_admin.servlet.MyFilter;
+import com.spring.l05_web_admin.servlet.MyServlet;
+
+// import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyRegistConfig {
+    @Bean
+    public ServletRegistrationBean myServlerRegistrationBean() {
+        return new ServletRegistrationBean<MyServlet>(new MyServlet(), "/my_servlet", "/my");
+    }
+
+    // @Bean
+    // public FilterRegistrationBean myFilterRegistrationBean() {
+
+    //     // return new FilterRegistrationBean<MyFilter>(new MyFilter(),
+    //     // this.myServlerRegistrationBean());
+    //     FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean<MyFilter>(new MyFilter());
+    //     filterRegistrationBean.setUrlPatterns(Arrays.asList("/css/*", "/images/*"));
+    //     return filterRegistrationBean;
+    // }
+}
--- l05_web_admin/src/main/java/com/spring/l05_web_admin/servlet/MyServlet.java
+++ l05_web_admin/src/main/java/com/spring/l05_web_admin/servlet/MyServlet.java
@@ -3,12 +3,12 @@ package com.spring.l05_web_admin.servlet;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+// import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/my_servlet")
+// @WebServlet(urlPatterns = "/my_servlet")
 public class MyServlet extends HttpServlet {
 
     @Override
