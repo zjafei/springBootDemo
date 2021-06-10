12ac4c87e34dcc1a6213ba1f8a9230ff44604f02
ericma <924402086@qq.com>
2021-06-10 17:13:47 +0800

    连接数据库完成

--- l05_web_admin/src/test/java/com/spring/l05_web_admin/L05WebAdminApplicationTests.java
+++ l05_web_admin/src/test/java/com/spring/l05_web_admin/L05WebAdminApplicationTests.java
@@ -1,13 +1,25 @@
 package com.spring.l05_web_admin;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootTest
 class L05WebAdminApplicationTests {
 
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
 	@Test
 	void contextLoads() {
+		// jdbcTemplate.queryForObject("select * from user", rowMapper);
+		// jdbcTemplate.queryForList("select * from user");
+		Long aLong = jdbcTemplate.queryForObject("select count(*) from innodb_index_stats", Long.class);
+        log.info("记录总数：{}", aLong);
 	}
 
 }
