9367ba37f0afa32eb89a221990c84e17c47126a4
ericma <924402086@qq.com>
2021-06-13 15:46:10 +0800

    使用 CityMapper.xml

--- l05_web_admin/src/main/java/com/spring/l05_web_admin/mapper/CityMapper.java
+++ l05_web_admin/src/main/java/com/spring/l05_web_admin/mapper/CityMapper.java
@@ -3,7 +3,7 @@ package com.spring.l05_web_admin.mapper;
 import com.spring.l05_web_admin.model.City;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
+// import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
@@ -11,7 +11,7 @@ public interface CityMapper {
     @Select("select * from city where id=#{id}")
     public City getById(Long id);
 
-    @Select("INSERT INTO city(`name`, `state`, `country`) VALUES(#{name}, #{state}, #{country})")
-    @Options(useGeneratedKeys = true, keyProperty = "id")
+    // @Select("INSERT INTO city(`name`, `state`, `country`) VALUES(#{name}, #{state}, #{country})")
+    // @Options(useGeneratedKeys = true, keyProperty = "id")
     public void insert(City city);
 }
