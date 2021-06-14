f128799a59764dfd6e374e7b74fc67c3e17d362a
ericma <924402086@qq.com>
2021-06-14 21:08:29 +0800

    添加了姓名的搜索

--- l05_web_admin/src/main/java/com/spring/l05_web_admin/controller/UserController.java
+++ l05_web_admin/src/main/java/com/spring/l05_web_admin/controller/UserController.java
@@ -2,6 +2,8 @@ package com.spring.l05_web_admin.controller;
 
 import javax.servlet.http.HttpSession;
 
+import com.alibaba.druid.sql.dialect.oracle.ast.clause.OracleStorageClause.FlashCacheType;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.spring.l05_web_admin.model.User;
 
 // import java.util.List;
@@ -11,6 +13,7 @@ import com.spring.l05_web_admin.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 // import org.springframework.web.bind.annotation.PathVariable;
@@ -47,19 +50,16 @@ public class UserController {
         }
     }
 
-    /**
-     * 获取用户列表
-     */
-    @GetMapping(value = { "/list" })
-    public String getList(
-            // @PathVariable("pageNo") Integer pageNo,
-            Model model, HttpSession session) {
-        System.out.println(session.getAttribute("message"));
-        model.addAttribute("message", session.getAttribute("message"));
-        session.removeAttribute("message");
+    @GetMapping(value = { "/delete" })
+    public String delete(@RequestParam(name = "id", required = true, defaultValue = "0") Long id, Model model,
+            HttpSession session) {
         model.addAttribute("activePage", "user");
-        model.addAttribute("users", userService.list());
-        return "table/user_table";
+        if (userService.removeById(id)) {
+            session.setAttribute("message", "用户删除成功!");
+        } else {
+            session.setAttribute("message", "用户删除sb!");
+        }
+        return "redirect:/user/list";
     }
 
     @GetMapping(value = { "/edit" })
@@ -87,15 +87,26 @@ public class UserController {
         }
     }
 
-    @GetMapping(value = { "/delete" })
-    public String delete(@RequestParam(name = "id", required = true, defaultValue = "0") Long id, Model model,
-            HttpSession session) {
+    /**
+     * 获取用户列表
+     */
+    @GetMapping(value = { "/list" })
+    public String getList(
+            @RequestParam(name = "keyword", required = false, defaultValue = "") String keyword,
+            Model model, HttpSession session) {
+        System.out.println(session.getAttribute("message"));
+        model.addAttribute("message", session.getAttribute("message"));
+        session.removeAttribute("message");
         model.addAttribute("activePage", "user");
-        if (userService.removeById(id)) {
-            session.setAttribute("message", "用户删除成功!");
-        } else {
-            session.setAttribute("message", "用户删除sb!");
+        model.addAttribute("keyword", keyword);
+
+        if(StringUtils.hasText(keyword)){
+            QueryWrapper<User> keyworkQueryWrapper = new QueryWrapper<>();
+            keyworkQueryWrapper.like("name", keyword);
+            model.addAttribute("users", userService.list(keyworkQueryWrapper));
+        }else{
+            model.addAttribute("users", userService.list());
         }
-        return "redirect:/user/list";
+        return "table/user_table";
     }
 }
